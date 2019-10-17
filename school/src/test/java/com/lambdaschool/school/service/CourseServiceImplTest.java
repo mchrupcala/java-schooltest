c963bd87d0f594c94c40ac1c134fd65e3b4e1c78
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-10-16 20:42:42 -0400

    Passing one test for findAll().

---
+++ school/src/test/java/com/lambdaschool/school/service/CourseServiceImplTest.java
@@ -0,0 +1,50 @@
+package com.lambdaschool.school.service;
+
+import com.lambdaschool.school.SchoolApplication;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SchoolApplication.class)
+public class CourseServiceImplTest {
+
+    @Autowired
+    private CourseService courseService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findAll() {
+        assertEquals(12, courseService.findAll().size());
+    }
+
+    @Test
+    public void getCountStudentsInCourse() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void findCourseById() {
+    }
+}
